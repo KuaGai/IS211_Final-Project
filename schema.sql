39cb35fb960e647e4013844b4aaeedc9c44eb5a2
KuaGai <32660710+KuaGai@users.noreply.github.com>
2019-12-01 01:46:01 -0500

    Add files via upload

---
+++ schema.sql
@@ -0,0 +1,26 @@
+DROP TABLE IF EXISTS Users;
+DROP TABLE IF EXISTS Posts;
+
+CREATE TABLE Users (
+    id INTEGER PRIMARY KEY,
+	username TEXT,
+    password TEXT
+);
+
+CREATE TABLE Posts (
+	id INTEGER PRIMARY KEY,
+	title TEXT,
+    username TEXT,
+    date TEXT,
+    last_update TEXT,
+    post TEXT,
+    is_published TEXT,
+    category TEXT
+);
+
+INSERT INTO Users
+(username, password)
+VALUES
+('Bajrush', 'tushi'),
+('Nika', 'kika'),
+('Kobe', 'bryant'),
